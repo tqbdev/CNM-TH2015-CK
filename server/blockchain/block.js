097e639edf0912052b7e29d23932c6aa7bedcae9
LAI NGOC BAO <1512025@student.hcmus.edu.vn>
2018-12-28 21:59:30 +0700

    update block

--- server/blockchain/block.js
+++ server/blockchain/block.js
@@ -1,3 +1,7 @@
+import * as CryptoJS from 'crypto-js';
+import {broadcastLatest} from './p2p';
+import {hexToBinary} from './util';
+
 class Block {
 
     public index: number;
@@ -5,21 +9,27 @@ class Block {
     public previousHash: string;
     public timestamp: number;
     public data: string;
+    public difficulty: number;
+    public nonce: number;
 
-    constructor(index: number, hash: string, previousHash: string, timestamp: number, data: string) {
+    constructor(index: number, hash: string, previousHash: string,
+                timestamp: number, data: string, difficulty: number, nonce: number) {
         this.index = index;
         this.previousHash = previousHash;
         this.timestamp = timestamp;
         this.data = data;
         this.hash = hash;
+        this.difficulty = difficulty;
+        this.nonce = nonce;
     }
 }
 
-const generateNextBlock = (blockData: string) => {
-    const previousBlock: Block = getLatestBlock();
-    const nextIndex: number = previousBlock.index + 1;
-    const nextTimestamp: number = new Date().getTime() / 1000;
-    const nextHash: string = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
-    const newBlock: Block = new Block(nextIndex, nextHash, previousBlock.hash, nextTimestamp, blockData);
-    return newBlock;
-};
+const genesisBlock: Block = new Block(
+    0, '91a73664bc84c0baa1fc75ea6e4aa6d1d20c5df664c724e3159aefc2e1186627', '', 1465154705, 'my genesis block!!', 0, 0
+);
+
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
