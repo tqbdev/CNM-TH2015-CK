30ced1cc3f901f2daec968e85d77f05b2656abfc
Tran Manh Chung <chungtran2801@gmail.com>
2018-12-31 22:31:29 +0700

    add api

--- NAIVECOIN/routes/index.js
+++ NAIVECOIN/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const { check } = require('express-validator/check')
 
+const Naivecoin = require('../middleware/naivecoin')
+
+const responseMiddleware = (req, res, next) => {
+  return res.json(req.responseValue)
+}
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Naive Coin' })
+})
 
-module.exports = router;
+router.post('/transactions/new', [
+  check('sender', 'Sender must be a String').exists(),
+  check('recipient', 'Sender must be a String').exists(),
+  check('amount', 'Sender must be a Int Value').isInt().exists()
+], Naivecoin.newTransaction, responseMiddleware)
+
+router.get('/mine', Naivecoin.mine, responseMiddleware)
+
+router.get('/chain', Naivecoin.getChain, responseMiddleware)
+
+router.post('/node/register', [
+  check('node', 'Node must be a String').exists()
+], Naivecoin.addNode, responseMiddleware)
+
+module.exports = router
