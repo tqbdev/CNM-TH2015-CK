afc6c5459e13b1e3db8129d62dc8e40b10f93d46
LAI NGOC BAO <1512025@student.hcmus.edu.vn>
2019-01-02 23:05:36 +0700

    update block v1.2

--- server/blockchain/block.js
+++ server/blockchain/block.js
@@ -44,3 +44,14 @@ const generateNextBlock = (blockData: string) => {
     broadcastLatest();
     return newBlock;
 };
+const calculateHashForBlock = (block: Block): string =>
+    calculateHash(block.index, block.previousHash, block.timestamp, block.data);
+
+const calculateHash = (index: number, previousHash: string, timestamp: number, data: string): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+const addBlock = (newBlock: Block) => {
+    if (isValidNewBlock(newBlock, getLatestBlock())) {
+        blockchain.push(newBlock);
+    }
+};
